6e8c7eee20f7d9d118f71bca689557a86f434c08
Vincent Yip <realsharkguy@gmail.com>
2024-08-12 16:51:07 -0400

    step value

--- app.py
+++ app.py
@@ -54,14 +54,14 @@ with col4:
 col1, col2 = st.columns(2)
 
 with col1:
-    enter_price = st.number_input('Entered Position Stock Price', min_value=0, value=100)
-    strike_price = st.number_input('Strike Price', min_value=0, value=100)
+    enter_price = st.number_input('Entered Position Stock Price', min_value=0, value=100, step=0.01)
+    strike_price = st.number_input('Strike Price', min_value=0, value=100, step=0.01)
     yr_out = datetime.datetime.now() + datetime.timedelta(days=7)
     expiration_date = st.date_input("Expiration Date", value=(yr_out))
 
 with col2:
-    premium = st.number_input('Option Premium', min_value=0, value=5)
-    shares = st.number_input('Number of Contracts', min_value=1, value=1)
+    premium = st.number_input('Option Premium', min_value=0, value=5, step=0.01)
+    shares = st.number_input('Number of Contracts', min_value=1, value=1, step=0.01)
 
 d = str(expiration_date)
 print(d)
@@ -72,10 +72,10 @@ annualized_yield=0
 
 if strategy == "Covered Call":
     yield_to_expiration = round((premium * 100) / enter_price, 2)
-    annualized_yield = round(yield_to_expiration / days_to_exp.days * 365, 2)  
+    annualized_yield = round((yield_to_expiration / days_to_exp.days) * 365, 2)  
 else:
     yield_to_expiration = round((premium * 100) / strike_price, 2)
-    annualized_yield = round(yield_to_expiration / days_to_exp.days * 365, 2)
+    annualized_yield = round((yield_to_expiration / days_to_exp.days) * 365, 2)
 
 col5, col6 = st.columns(2)
 
